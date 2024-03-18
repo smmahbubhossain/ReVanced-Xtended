c617433bc1c437f958a3c6cbd7c3ec3000b873ff
S M Mahbub Hossain <118460787+smmahbubhossain@users.noreply.github.com>
2024-03-18 18:26:00 +0600

    Update customize.sh
    Signed-off-by: S M Mahbub Hossain  <118460787+smmahbubhossain@users.noreply.github.com>

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -119,5 +119,5 @@ for s in "uninstall.sh" "service.sh"; do
 done
 
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by smmahbubhossain (github.com/smmahbubhossain)"
 ui_print " "
