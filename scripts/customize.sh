1b2046e30b039b73ec97f9fffb9fa3f8c626b275
S M Mahbub Hossain <118460787+smmahbubhossain@users.noreply.github.com>
2024-03-28 09:46:21 +0600

    Update customize.sh
    Signed-off-by: S M Mahbub Hossain  <118460787+smmahbubhossain@users.noreply.github.com>

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -140,5 +140,12 @@ for s in "uninstall.sh" "service.sh"; do
 done
 
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by smmahbubhossain (github.com/smmahbubhossain)"
+ui_print "  thanks to ReVanced Team (github.com/ReVanced)"
+ui_print "  thanks to inotia00 (github.com/inotia00)"
+ui_print "  thanks to j-hc (github.com/j-hc)"
 ui_print " "
+
+sleep 0.5
+
+ui_print "  No need to reboot your phone"
